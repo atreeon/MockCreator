85f2fa345593cc5b768cd9bcf38e36e12a1430d0
Atree <atreeonhill@gmail.com>
2023-06-08 13:48:57 +0100

    updated reference to mock_creator_annotation

--- mock_creator_generator/lib/src/MockCreatorGenerator.dart
+++ mock_creator_generator/lib/src/MockCreatorGenerator.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:generator_common/NameType.dart';
-import 'package:generator_common/helpers.dart';
 import 'package:mock_creator_annotation/mock_creator_annotation.dart';
 import 'package:mock_creator_generator/src/createMockCreator.dart';
 import 'package:source_gen/source_gen.dart';
